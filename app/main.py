14ca19d1c3c3dcdbec8168911e047cdc716fc17c
Rafhael de Oliveira Martins <rafhaeldeoliveira@gmail.com>
2023-07-07 19:03:46 -0300

    :sparkles: add: Novas funções de pré-processamento.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from .functions import extract_news, load_tokenizer_summary, load_model_summary, load_vectorizer_classification, load_model_classification
-from .schemas import ResumoTextoInput, ClassificacaoTextoInput, ResumoUrlInput, ClassificacaoUrlInput
+from .functions import extract_news, load_tokenizer_summary, load_model_summary, load_vectorizer_classification, load_model_classification, limpa_frases_texto, limpa_frases_titulo, rouge_metrica, normalizer
+from .schemas import ClassificacaoTextoInput, ResumoUrlInput, ClassificacaoUrlInput
 
 app = FastAPI()
 
@@ -12,49 +12,69 @@ async def home():
         content={"message": "Summarization Abstractive and Classified Headline Service"}
     )
 
-@app.post("/resumo/texto/")
-async def ResumoTexto(texto_noticia_request: ResumoTextoInput):
-    text = texto_noticia_request.texto_noticia
-    inputs = tokenizer.encode(text, max_length=3000, truncation=True, return_tensors='pt')
-    summary_ids = model.generate(inputs, max_length=100, min_length=32, num_beams=5, no_repeat_ngram_size=3, early_stopping=True)
-    summary = tokenizer.decode(summary_ids[0])
+@app.post("/resumo/")
+async def ResumoTexto(url_noticia_request: ResumoUrlInput):
+    url = url_noticia_request.url_noticia
+    titulo, texto = extract_news(url)
+    texto = limpa_frases_texto(texto)
+    titulo = limpa_frases_titulo(titulo)
+
+    tokenizer_summary = load_tokenizer_summary()
+    model_summary = load_model_summary()
+
+    inputs = tokenizer_summary.encode(texto, max_length=3000, truncation=True, return_tensors='pt')
+    summary_ids = model_summary.generate(inputs, max_length=100, min_length=32, num_beams=5, no_repeat_ngram_size=3, early_stopping=True)
+    summary = tokenizer_summary.decode(summary_ids[0])
 
     manchete = summary.replace('<pad> ', '').replace('</s>','')
-    score = texto_noticia_request.score
+
+    rouge_metrica_score = rouge_metrica()
+    score = rouge_metrica_score.score(titulo, manchete)
+    
     return JSONResponse(
         status_code=200,
-        content={'manchete': manchete,
-                 'score': score}
+        content={'manchete_original': titulo,
+                'manchete_gerada': manchete,
+                'score': score}
     )
 
-@app.post("/classificacao/texto/")
+@app.post("/classificacao/texto")
 async def ClassificacaoTexto(texto_manchete_request: ClassificacaoTextoInput):
-    tema = texto_manchete_request.manchete_noticia
-    score = 100
-    return JSONResponse(
-        status_code=200,
-        content={'tema': tema,
-                 'score': score}
-    )
+    titulo_noticia = texto_manchete_request.manchete_noticia
 
-@app.post("/resumo/url/")
-async def ResumoUrl(url_noticia_request: ResumoUrlInput):
-    url = url_noticia_request.url_noticia
-    score = 100
+    vectorizer_classification = load_vectorizer_classification() 
+    model_classification = load_model_classification()
+
+    tema_pred = model_classification.predict(vectorizer_classification.transform([normalizer(titulo_noticia)]))[0]
+    tema_pred_proba = model_classification.predict(vectorizer_classification.transform([normalizer(titulo_noticia)]))
+    tema_model = model_classification.classes_
+
     return JSONResponse(
         status_code=200,
-        content={'url_noticia': url,
-                 'score': score}
+        content={'titulo': titulo_noticia,
+                'tema_predito': tema_pred,
+                'score': tema_pred_proba,
+                'score_class': tema_model}
     )
 
 @app.post("/classificacao/url/")
 async def ClassificacaoUrl(classificacaourl_request: ClassificacaoUrlInput):
     url = classificacaourl_request.url_noticia
-    titulo = extract_news(url)
-    score = 100
+    titulo, texto = extract_news(url)
+    texto = limpa_frases_texto(texto)
+    titulo_noticia = limpa_frases_titulo(titulo)
+
+    vectorizer_classification = load_vectorizer_classification() 
+    model_classification = load_model_classification()
+
+    tema_pred = model_classification.predict(vectorizer_classification.transform([normalizer(titulo_noticia)]))[0]
+    tema_pred_proba = model_classification.predict(vectorizer_classification.transform([normalizer(titulo_noticia)]))
+    tema_model = model_classification.classes_
+
     return JSONResponse(
         status_code=200,
-        content={'url_noticia': url,
-                 'titulo_noticia': titulo,
-                 'score': score}
+        content={'titulo': titulo_noticia,
+                'tema_predito': tema_pred,
+                'score': tema_pred_proba,
+                'score_class': tema_model}
     )
